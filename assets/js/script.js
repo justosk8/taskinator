78d87acd24b4aa93539b2b187b8f407c9ea8b0ce
Ed Abbey <websterhomer78@gmail.com>
2021-02-10 11:31:12 -0700

    added reset to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
 
 
-    //Rename the handler function to be a little more specific to the event it's handling.
-
-    //Create a new function to take in the task's name and title as arguments and create the HTML elements that get added to the page.
-
-    //Move the code that creates and adds HTML elements from the handler function into the newly created function.
-
-    //Update the handler function to send the task name and type values from the form to the newly created function.
-
+   
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
@@ -17,6 +10,12 @@ var taskFormHandler = function(event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
